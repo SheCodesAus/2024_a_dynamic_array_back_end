0f16d8686d28970d97ef945c2677b29169aea822
Rachel Blake <blakerach1@gmail.com>
2024-03-20 14:59:14 +0800

    migrations run, db setup, including new user model

--- DiversiTech/DiversiTech/settings.py
+++ DiversiTech/DiversiTech/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
