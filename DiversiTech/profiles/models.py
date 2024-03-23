33d3fa418d482deedb2e2c74055ead362b409bd4
anyab <anyabelkina123@gmail.com>
2024-03-23 09:52:39 +0800

    created Profile List and Profile Detail views. ProfileDetail view has GET and POST commands. ProfileDetail view has GET, PUT and DELETE methods. ProfileDetail has IsOwnerorReadOnly permissions. Urls are taking id for now, will need to figure out how to change it to th eusername

--- DiversiTech/profiles/models.py
+++ DiversiTech/profiles/models.py
@@ -3,29 +3,29 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
   owner = models.CharField(max_length=200)
-  first_name = models.CharField(max_length=30, null=False)
-  last_name = models.CharField(max_length=30, null=False)
-  username = models.CharField(max_length=30, null=False)
-  bio = models.CharField(max_length=1800, null=False)
-  location = models.JSONField()
-  picture_url = models.URLField()
-  is_hidden = models.BooleanField()
-  number_of_endorsements = models.IntegerField()
-  email_url = models.EmailField()
-  facebook_url = models.URLField()
-  instagram_url = models.URLField()
-  github_url = models.URLField()
-  linked_in_url = models.URLField()
-  portfolio_url = models.URLField()
-  contact_preference_selection = (
+  first_name = models.CharField(max_length=30,null=False, blank=False)
+  last_name = models.CharField(max_length=30,null=False, blank=False)
+  username = models.CharField(max_length=30,null=False, blank=False)
+  bio = models.CharField(max_length=1800, null=False, blank=False)
+  location = models.JSONField(null=True, blank=True)
+  picture_url = models.URLField(null=True, blank=True)
+  is_hidden = models.BooleanField(null=True, blank=True)
+  number_of_endorsements = models.IntegerField(null=True, blank=True)
+  email_url = models.EmailField(null=True, blank=True)
+  facebook_url = models.URLField(null=True, blank=True)
+  instagram_url = models.URLField(null=True, blank=True)
+  github_url = models.URLField(null=True, blank=True)
+  linkedin_url = models.URLField(null=True, blank=True)
+  portfolio_url = models.URLField(null=True, blank=True)
+  contact_preference = (
         ("Contact Form", "Contact Form"),
         ("Email", "Email"),
         ("LinkedIn", "LinkedIn"),
         ("Facebook", "Facebook"),
         ("Instagram", "Instagram")
     )
-  is_open_to_mentor = models.BooleanField()
-  is_seeking_mentorship = models.BooleanField()
-  date_created = models.DateTimeField()
+  is_open_to_mentor = models.BooleanField(null=True, blank=True)
+  is_seeking_mentorship = models.BooleanField(null=True, blank=True)
+  date_created = models.DateTimeField(null=True, blank=True)
 
- 
+  
