6bce2b3b4e99cae0528e8502829e6ea9ae248928
anyab <anyabelkina123@gmail.com>
2024-03-24 17:34:10 +0800

    updated Profile model and added Permissions.py code

--- DiversiTech/profiles/models.py
+++ DiversiTech/profiles/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
   owner = models.CharField(max_length=200)
-  first_name = models.CharField(max_length=30,null=False, blank=False)
-  last_name = models.CharField(max_length=30,null=False, blank=False)
-  username = models.CharField(max_length=30,null=False, blank=False)
+  # first_name = models.CharField(max_length=30,null=False, blank=False)
+  # last_name = models.CharField(max_length=30,null=False, blank=False)
+  # username = models.CharField(max_length=30,null=False, blank=False)
   bio = models.CharField(max_length=1800, null=False, blank=False)
-  location = models.JSONField(null=True, blank=True)
+  location = models.CharField(max_length=50,null=True, blank=True)
   picture_url = models.URLField(null=True, blank=True)
   is_hidden = models.BooleanField(null=True, blank=True)
   number_of_endorsements = models.IntegerField(null=True, blank=True)
@@ -17,13 +17,14 @@ class Profile(models.Model):
   github_url = models.URLField(null=True, blank=True)
   linkedin_url = models.URLField(null=True, blank=True)
   portfolio_url = models.URLField(null=True, blank=True)
-  contact_preference = (
+  contact_preference_choices = (
         ("Contact Form", "Contact Form"),
         ("Email", "Email"),
         ("LinkedIn", "LinkedIn"),
         ("Facebook", "Facebook"),
         ("Instagram", "Instagram")
     )
+  contact_preference = models.CharField(max_length=30, choices=contact_preference_choices)
   is_open_to_mentor = models.BooleanField(null=True, blank=True)
   is_seeking_mentorship = models.BooleanField(null=True, blank=True)
   date_created = models.DateTimeField(null=True, blank=True)
