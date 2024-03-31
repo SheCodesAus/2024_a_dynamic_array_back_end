125d787b71fa37f99558aeed452a47443cfaa785
anyab <anyabelkina123@gmail.com>
2024-03-31 15:11:45 +0800

    changed debug filed in settings.py to false 2

--- DiversiTech/DiversiTech/settings.py
+++ DiversiTech/DiversiTech/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get(
     'DJANGO_DEBUG',
-    'True'
+    'False'
 ) != 'False'
 # gotta remember this!
 
